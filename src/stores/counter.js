ab61ebf1b1082085b01aa6f7efa4070344743ee7
kiou <quentin.far@gmail.com>
2022-08-01 18:07:26 +0200

    ameliorations

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -199,7 +199,7 @@ if(this.responseArray.includes(id)){
         }
         else if(totalResponseArray==totalQuestionArray&&lenQuestion==lenResponse){
             this.vueResult=`Vous avez raison ${this.question.name} sont récoltés  ${this.textResult}`
-            this.vueResultClass='mb-2 font-bold tracking-tight text-green-800 dark:text-green-800'
+            this.vueResultClass='mb-2 font-bold tracking-tight text-green-700 dark:text-green-700'
           console.log(this.fruits)
           this.finalResult.push(2)
           this.intFinalResult=this.finalResult.reduce((a,b)=>a+b)
